708682f187f129615939b5fd8f6f73537c5f5514
Reyhan Koyun <reyhanzehrakoyun@gmail.com>
2025-11-17 13:42:08 -0800

    Remove the empty VersionInfo.cs file

--- src/Amazon.SecretsManager.Extensions.Caching/VersionInfo.cs
+++
@@ -1,2 +0,0 @@
-// This file is auto-generated during build from PackageVersion in the .csproj file.
-// Do not edit manually. See GenerateVersionInfo target in Amazon.SecretsManager.Extensions.Caching.csproj
